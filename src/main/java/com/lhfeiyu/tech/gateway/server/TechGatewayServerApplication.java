3f46a613638f03a065745e9b6ca95ba7a202ad75
yurh <airson_yu@163.com>
2019-06-18 10:40:02 +0800

    init

---
+++ src/main/java/com/lhfeiyu/tech/gateway/server/TechGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.lhfeiyu.tech.gateway.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechGatewayServerApplication.class, args);
+    }
+
+}

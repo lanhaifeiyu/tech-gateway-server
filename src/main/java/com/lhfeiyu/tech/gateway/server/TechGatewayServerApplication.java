c58c5e05634e8d82a64863334eaf3658b37b5232
yurh <airson_yu@163.com>
2019-06-20 23:29:00 +0800

    t

--- src/main/java/com/lhfeiyu/tech/gateway/server/TechGatewayServerApplication.java
+++ src/main/java/com/lhfeiyu/tech/gateway/server/TechGatewayServerApplication.java
@@ -2,9 +2,20 @@ package com.lhfeiyu.tech.gateway.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TechGatewayServerApplication {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/google").uri("http://google.com:80/").id("google_route")
+                ).build();
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TechGatewayServerApplication.class, args);

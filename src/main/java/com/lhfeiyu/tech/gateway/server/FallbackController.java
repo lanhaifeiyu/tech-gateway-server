f2d7a0687b60bb89a55207222671f372a2935bd8
yurh <airson_yu@163.com>
2019-06-23 23:20:50 +0800

    feign gateway

---
+++ src/main/java/com/lhfeiyu/tech/gateway/server/FallbackController.java
@@ -0,0 +1,14 @@
+package com.lhfeiyu.tech.gateway.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback")
+    public String fallback() {
+        return "Spring Cloud Gateway Fallback！";
+    }
+
+}
--- src/main/java/com/lhfeiyu/tech/gateway/server/TechGatewayServerApplication.java
+++ src/main/java/com/lhfeiyu/tech/gateway/server/TechGatewayServerApplication.java
@@ -2,20 +2,27 @@ package com.lhfeiyu.tech.gateway.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class TechGatewayServerApplication {
 
-    @Bean
+    //https://segmentfault.com/a/1190000019074715
+
+    /*@Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/google").uri("http://google.com:80/").id("google_route")
+                //basic proxy
+                .route(r -> r.path("/jd").uri("http://baidu.com:80/").id("jd_route")
                 ).build();
 
-    }
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(TechGatewayServerApplication.class, args);
